f41228b65db13d8d950868efaabb84faacaa3d52
Maud @mihani <maud.remoriquet@gmail.com>
2020-10-03 12:07:06 +0200

    Add several package

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+import './styles/global.scss';
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
